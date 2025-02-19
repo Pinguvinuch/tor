557561b30d127dea09bf3516ee00a46764610926
Andrey Dorodhykh <deripapaivan@gmail.com>
2025-02-19 19:16:57 +0200

    commit10

--- Teamwork/Program.cs
+++ Teamwork/Program.cs
@@ -2,35 +2,5 @@
 Console.WriteLine("roma");
 Console.WriteLine("Андрей");
 Console.WriteLine("imposter is green");
-
-Console.WriteLine("roma");
-Console.WriteLine("Андрей");
-int lol = 109;
-Console.WriteLine(lol);
-
-Console.WriteLine("roma");
-Console.WriteLine("Андрей");
-Console.WriteLine("imposter is green");
-
-Console.WriteLine("roma");
-Console.WriteLine("Андрей");
-int ll = 1319;
-Console.WriteLine(ll);
-
-Console.WriteLine("roma");
-Console.WriteLine("Андрей");
-Console.WriteLine("imposter is green");
-
-Console.WriteLine("roma");
-Console.WriteLine("Андрей");
-int lo = 100;
-Console.WriteLine(lo);
-
-Console.WriteLine("roma");
-Console.WriteLine("Андрей");
-Console.WriteLine("imposter is green");
-
-Console.WriteLine("roma");
-Console.WriteLine("Андрей");
 int ol = 19;
 Console.WriteLine(ol);
